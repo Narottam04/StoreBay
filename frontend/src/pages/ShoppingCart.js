5a305a81a8e93c9d266cf8d0d0849497692b05a7
ZaidKhan2002 <zk76159@gmail.com>
2022-01-08 20:12:11 +0530

    Solved cart issue for mobile size

--- frontend/src/pages/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart.js
@@ -89,9 +89,40 @@ function ShoppingCart() {
                                             </ul>  
                                         )
                                     }
+                                     <div className="xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full md:hidden p-0">
+                                    <div className=" flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
+                                        <div>
+                                            <p className="text-4xl font-black leading-9 text-gray-800">Summary</p>
+                                            <div className="flex items-center justify-between pt-16">
+                                                <p className="text-base leading-none text-gray-800">Subtotal</p>
+                                                <p className="text-base leading-none text-gray-800">${totalCartPrice}
+                                                </p>
+                                            </div>
+                                            <div className="flex items-center justify-between pt-5">
+                                                <p className="text-base leading-none text-gray-800">Shipping</p>
+                                                <p className="text-base leading-none text-gray-800">${
+                                                    totalCartPrice > 100 ? 0 : 10
+                                                }</p>
+                                            </div>
+                                            {/* <div className="flex items-center justify-between pt-5">
+                                                <p className="text-base leading-none text-gray-800">Tax</p>
+                                                <p className="text-base leading-none text-gray-800">$5</p>
+                                            </div> */}
+                                        </div>
+                                        <div>
+                                            <div className="flex items-center pb-6 justify-between lg:pt-5 pt-20">
+                                                <p className="text-2xl leading-normal text-gray-800">Total</p>
+                                                <p className="text-2xl font-bold leading-normal text-right text-gray-800">${totalCartPrice}</p>
+                                            </div>
+                                            <button disabled={cartItems.length === 0} onClick={checkoutHandler}  className="text-base leading-none w-full py-5 bg-gray-800 border-gray-800 border focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-white">
+                                                {cartItems.length === 0 ? "Add Items To Cart" : "Checkout"}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                                 </div>
                                 {/* Cart summary */}
-                                <div className="xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full">
+                                <div className="xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full hidden md:block">
                                     <div className=" flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
                                         <div>
                                             <p className="text-4xl font-black leading-9 text-gray-800">Summary</p>
