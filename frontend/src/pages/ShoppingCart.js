2a5a34c158ecac34e66c6f03cba11a49c9472e03
ZaidKhan2002 <zk76159@gmail.com>
2022-01-09 16:35:33 +0530

    Added Profile Page and fixed cart in mobile size

--- frontend/src/pages/ShoppingCart.js
+++ frontend/src/pages/ShoppingCart.js
@@ -40,7 +40,7 @@ function ShoppingCart() {
                 <div className="lora-font w-full h-full bg-black bg-opacity-90 top-0 overflow-y-auto overflow-x-hidden fixed sticky-0" id="chec-div">
                     <div className="w-full absolute z-10 right-0 h-full overflow-x-hidden transform translate-x-0 transition ease-in-out duration-700" id="checkout">
                         <div className="flex md:flex-row flex-col justify-end" id="cart">
-                            <div className="w-full md:pl-10 pl-4 pr-10 md:pr-4 md:py-12 py-8 bg-white overflow-y-auto overflow-x-hidden h-screen" id="scroll">
+                            <div className="w-full md:pl-10 md:pr-4 md:py-12 bg-white overflow-y-auto overflow-x-hidden h-screen" id="scroll">
                                 <Link className="flex items-center text-gray-500 hover:text-gray-600 cursor-pointer" to="/" >
                                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-left" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -59,18 +59,18 @@ function ShoppingCart() {
                                             <ul>
                                                 {
                                                     cartItems.map(item => (
-                                                    <li key={item.product} className="md:flex items-center mt-14 py-8 border-t border-gray-200">
-                                                        <Link to={`/product/${item.product}`}  className="w-60 m-auto md:m-2 ">
+                                                    <li key={item.product} className="flex items-center mt-14 py-8 border-t border-gray-200">
+                                                        <Link to={`/product/${item.product}`}  className="w-60 m-auto m-2 ">
                                                             <img src={item.image} alt className="w-full h-full object-center object-cover" />
                                                         </Link>
-                                                        <div className="md:pl-3 md:w-3/4">
+                                                        <div className="pl-3 w-3/4">
                                                             {/* <p className="text-xs leading-3 text-gray-800 md:pt-0 pt-4">RF293</p> */}
-                                                            <div className="flex items-center justify-between w-full pt-1">
-                                                                <Link to={`/product/${item.product}`} className="text-base font-black leading-none text-gray-800">{item.name}</Link>
-                                                                <p className="py-2 px-1  mr-6 focus:outline-none">
+                                                            <div className="flex items-center justify-between w-full pt-1 flex-initial">
+                                                                <Link to={`/product/${item.product}`} className="text-base font-black leading-none text-gray-800 w-44">{item.name}</Link>
+                                                            </div>
+                                                            <p className="md:py-2 md:px-1  md:mr-6 mt-4 focus:outline-none whitespace-pre ">
                                                                   Quantity: {item.qty}  
                                                                 </p>
-                                                            </div>
                                                             <p className=" text-xs leading-3 text-gray-600 pt-4">Brand: {item.brand}</p>
                                                             <p className="text-xs leading-3 text-gray-600 pb-4 pt-2">Category: {item.category}</p>
                                                             {/* <p className="w-96 text-xs leading-3 text-gray-600">Composition: 100% calf leather</p> */}
@@ -89,7 +89,7 @@ function ShoppingCart() {
                                             </ul>  
                                         )
                                     }
-                                     <div className="xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-full md:hidden p-0">
+                                     <div className="xl:w-1/2 md:w-1/3 xl:w-1/4 w-full bg-gray-100 h-84 md:hidden p-0">
                                     <div className=" flex flex-col md:h-screen px-14 py-20 justify-between overflow-y-auto">
                                         <div>
                                             <p className="text-4xl font-black leading-9 text-gray-800">Summary</p>
