4f74331c7eb9bd0308de749c2584aa6f3e33c84f
Narottam04 <coutkunal@gmail.com>
2022-01-11 20:51:06 +0530

    backend and frontend for initial admin page

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,6 +103,67 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
   })
   
+// @desc    Get all users
+// @route   GET /api/users/
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+// @desc    Delete a user
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const deleteUsers = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if(user){
+    await user.remove()
+    res.json({message:"User Removed"})
+  }
+  else{
+    res.status(404)
+    throw new Error('User Not found')
+  }
+})
+
+// @desc    Get  user by id
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  }
+  else {
+    res.status(404)
+    throw new Error('User Not found')
+  }
+})
+
+// @desc    update user 
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin 
+
+    const updatedUser = await user.save()
+
+  res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email:updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+  })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 
-export {authUser,getUserProfile,registerUser,updateUserProfile}
+export {authUser,getUserProfile,registerUser,updateUserProfile,getUsers,deleteUsers,getUserById,updateUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import { authUser ,getUserProfile,registerUser,updateUserProfile} from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser ,deleteUsers,getUserById,getUserProfile,getUsers,registerUser,updateUser,updateUserProfile} from '../controllers/userController.js'
+import { protect,admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-router.route('/').post(registerUser) 
+router.route('/').post(registerUser).get(protect,admin,getUsers) 
 
 router.post('/login', authUser)
 
 router.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile)
 
-
+router.route('/:id').delete(protect,admin,deleteUsers).get(protect,admin,getUserById).put(protect,admin,updateUser)
 export default router
